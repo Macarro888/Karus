192ce05f0de1676be9b121241cb44f1ab130d93e
Macarro888 <galaxyj2002@gmail.com>
2023-12-06 13:57:52 +0100

    - Aggiunto la classe "NPC_CIVILE"   1. Aggiunto variabile "nome";   2. Aggiunto variabile "punti_vita";   3. Aggiunto variabile "occupazione";   4. Aggiunto variabile "denaro";   5. Aggiunto metodo "presentati";   6. Aggiunto metodo "Risposta_buono";   7. Aggiunto metodo "Risposta_fredda";   8. Aggiunta meotod "Risposta_cattiva";   9. Aggiunto variabile "relazione";

--- src/NPC/NPC_CIVILI.java
+++ src/NPC/NPC_CIVILI.java
@@ -1,4 +1,35 @@
 package NPC;
 
 public class NPC_CIVILI {
+    private String nome;
+    private int punti_vita;
+    private String occupazione;
+    private int denaro;
+    private int relazione;
+
+    public NPC_CIVILI(String nome, int punti_vita, String occupazione, int denaro, int relazione) {
+        this.nome = nome;
+        this.punti_vita = punti_vita;
+        this.occupazione = occupazione;
+        this.denaro = denaro;
+        this.relazione = relazione;
+    }
+
+    public void presentati (){
+        System.out.println("Salve viandante, mi chiamo "+nome+" invece tu come ti chiami?");
+    }
+
+    public void Risposta_buona(String nome_giocatore){
+        System.out.println("Piacere mio "+nome_giocatore);
+        relazione = relazione + 2;
+    }
+
+    public void Risposta_fredda(){
+        System.out.println("Mh...");
+    }
+
+    public void Risposta_cattiva(){
+        System.out.println("Che maleducato!");
+        relazione = relazione - 2;
+    }
 }
